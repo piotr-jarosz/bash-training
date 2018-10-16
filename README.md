8115833785a8fbcaaab629646d8890469b465254
Piotr Jarosz <piotr.jarosz@wonga.com>
2018-10-16 03:04:50 +0200

    * use rsync to perform the actual backup. * offer a choice of tar or rsync. * copy to a folder named for the current day of the week * run once daily * use tar to perform the actual backup.

--- README.md
+++ README.md
@@ -7,12 +7,25 @@ This is first project due to training bash skills
 A script to backup configured directories.
 
 ### Requirements 
-* run once daily
-* copy to a folder named for the current day of the week
 * keeping 7 daily backups (including the current day) as history 
-* either use rsync or tar to perform the actual backup.
+* handle configuration file
+* handle parameters
+* override config file throught parameters
 
 ### Future Enhancements:
 * check for disk space before attempting the backup and pause the script, email an alert to you and then wait until sufficient disk space is available.
 * write you a report showing start time, end time and quantity of data copied.
+* error handler.
+* config check
+* possibility backup to ftp
+* possibility backup to sftp
+* possibility backup to s3
+* possibility backup to NFS volume
+* Installation (create custom config file, initially add script to crontab and setup some installation directory)
+
+### Done
+* use rsync to perform the actual backup.
 * offer a choice of tar or rsync.
+* copy to a folder named for the current day of the week
+* run once daily
+* use tar to perform the actual backup.
--- body.sh
+++ body.sh
@@ -6,6 +6,14 @@ debug() {
 	[[ $DEBUG -eq 1 ]] && echo "$@"
 }
 
+# SCRIPT SPECIFIC VARIABLES
+# if not empty, will override file config variables
+
+SOURCE_PATH="/tmp/dotestow"
+DESTINATION_PATH="/tmp/backuptest"
+COPY_TYPE="rsync"
+DEST_PATTERN='as+%F-%A'
+
 # DEFAULTS
 CURRENT_PATH="$(pwd -P)"
 debug "CURRNET PATH: $CURRENT_PATH"
@@ -13,7 +21,19 @@ SCRIPT_PATH=`cd $(dirname "$0") ; pwd -P`
 debug "SCRIPT PATH: $SCRIPT_PATH"
 SCRIPT_NAME=`basename $0`
 debug "SCRIPT NAME: $SCRIPT_NAME"
-CRON_PATH=/etc/cron.daily/
+#CRON_PATH=/etc/cron.daily
+COPY_TYPES="cp, tar, targz, rsync"
+#variables check
+[[ $COPY_TYPES = *$COPY_TYPE* ]] || debug "WRONG COPY_TYPE: $COPY_TYPE, shoul be one of: $COPY_TYPES"
+[[ -z $CONF_FILE ]] && CONF_FILE=${SCRIPT_PATH}/base.conf
+debug "CONF_FILE: $CONF_FILE"
+[[ -e $CONF_FILE ]] && (debug "FILE EXISTS: $CONF_FILE"; source $CONF_FILE) || debug "FILE NOT EXISTS: $CONF_FILE"
+[[ -z $DEST_NAME ]] && DEST_NAME=`date +"$DEST_PATTERN"`; debug "DEST_NAME: $DEST_NAME"
+[[ -z $DESTINATION_PATH ]] && debug "error Please setup destination path"
+
+
+
+
 
 # run once daily
 cron() {
@@ -26,4 +46,33 @@ cron() {
 	fi 
 }
 
+backup() {
+	[[ -d $DESTINATION_PATH ]] && debug "DESTINATION PATH EXISTS: $DESTINATION_PATH" || (mkdir -p $DESTINATION_PATH && debug "DESTINATION PATH CREATED: $DESTINATION_PATH")
+	if [[ $COPY_TYPE == "tar" ]]; then
+		debug "STARTING TAR"
+		DESTINATION_PATH="$DESTINATION_PATH/$DEST_NAME.tar"
+		tar -cvf "$DESTINATION_PATH" -C "$SOURCE_PATH" .
+	elif [[ $COPY_TYPE == "targz" ]]; then
+		debug "STARTING TARGZ"
+		DESTINATION_PATH="$DESTINATION_PATH/$DEST_NAME.tar.gz"
+		tar -zcvf "$DESTINATION_PATH" -C "$SOURCE_PATH" .
+	elif [[ $COPY_TYPE == "cp" ]]; then
+		debug "STARTING CP"
+		DESTINATION_PATH="$DESTINATION_PATH/$DEST_NAME"
+		mkdir -p $DESTINATION_PATH
+		cp -R --verbose $SOURCE_PATH/* $DESTINATION_PATH/
+	elif [[ $COPY_TYPE == "rsync" ]]; then
+		debug "STARTING RSYNC"
+		DESTINATION_PATH="$DESTINATION_PATH/$DEST_NAME"
+		mkdir -p $DESTINATION_PATH
+		rsync -v $SOURCE_PATH/ $DESTINATION_PATH/
+	fi
+
+}
+
+
+
+
+
+cd $CURRENT_PATH
 "$@"
